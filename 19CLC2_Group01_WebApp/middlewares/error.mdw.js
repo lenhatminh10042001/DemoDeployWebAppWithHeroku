31e43b8becc92f4a0e1229232322c4b0b3f3f68d
lenhatminh10042001 <64116754+lenhatminh10042001@users.noreply.github.com>
2022-01-11 02:23:32 +0700

    Comment err

--- 19CLC2_Group01_WebApp/middlewares/error.mdw.js
+++ 19CLC2_Group01_WebApp/middlewares/error.mdw.js
@@ -1,9 +1,9 @@
 //handling error.
 export default function(app) {
-    //500
-    app.use(function(err, req, res, next) {
-        res.render('500', {layout: false})
-    })
+    // //500
+    // app.use(function(err, req, res, next) {
+    //     res.render('500', {layout: false})
+    // })
     //404
     app.use(function(req, res, next) {
         res.render('404', {layout: false})
